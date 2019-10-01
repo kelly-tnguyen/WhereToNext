eae9419da0ab2826fa85c6bc141199a88f0de1de
Kelly T Nguyen <Kelly.Nguyen@medspring.local>
2019-10-01 04:38:16 -0500

    added some stuff that didn't work but i want it

--- src/components/ParkList.js
+++ src/components/ParkList.js
@@ -10,8 +10,157 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { wrap } from 'module';
 
+//THIS WORKS 
+
+const useStyles = makeStyles({
+  image: {
+    maxHeight: 200,
+    maxWidth: 250,
+  },
+  card: {
+    maxWidth: 300,
+    height: 350,
+    margin: 10,
+    wordWrap: wrap
+  },
+  media: {
+    height: 180,
+    width: 300
+  },
+});
+
+export default function MediaCard(props) {
+  const classes = useStyles();
+  
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image = {props.image}
+          />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.location}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Add to Next Adventure
+        </Button>
+        <Button size="small" color="primary">
+          Archive
+        </Button>
+      </CardActions>
+    </Card>
+  );
+  }
+
+//ENDS
 
 
+//THIS IS WHAT I WOULD LIKE BUT DOESNT WORK 
+
+// import React from "react";
+// import Card from "@material-ui/core/Card";
+// import CardMedia from "@material-ui/core/CardMedia";
+// import CardContent from "@material-ui/core/CardContent";
+// import Link from "@material-ui/core/Link";
+// import Typography from "@material-ui/core/Typography";
+ 
+
+// const NewsCard02 = (props) => (
+//   <Card className={"MuiNewsCard--02"}>
+//     <CardMedia
+//       component={"img"}
+//       className={"MuiCardMedia-root"}
+//       image={props.image}
+//     />
+//     <CardContent className={"MuiCardContent-root"}>
+//       <Typography
+//         className={"MuiTypography--heading"}
+//         color={"inherit"}
+//         variant={"h3"}
+//         gutterBottom
+//       >
+//         {props.name}
+//       </Typography>
+//       <Typography className={"MuiTypography--subheading"} color={"inherit"}>
+//         {props.location}
+//       </Typography>
+//       <Typography
+//         className={"MuiTypography--explore"}
+//         color={"inherit"}
+//         variant={"caption"}
+//       >
+//         <Link color={"inherit"} underline={"none"}>
+//           ADD TO NEXT ADVENTURE
+//         </Link>
+//       </Typography>
+//     </CardContent>
+//   </Card>
+// );
+
+// NewsCard02.getTheme = muiBaseTheme => ({
+//   MuiCard: {
+//     root: {
+//       "&.MuiNewsCard--02": {
+//         maxWidth: 304,
+//         margin: "auto",
+//         position: "relative",
+//         transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
+//         boxShadow: "0 16px 40px -12.125px rgba(0,0,0,0.3)",
+//         borderRadius: 0,
+//         "&:hover": {
+//           "& .MuiTypography--explore": {
+//             transform: "scale(1.2)"
+//           }
+//         },
+//         "& button": {
+//           marginLeft: 0
+//         },
+//         "& .MuiCardMedia-root": {
+//           height: "100%"
+//         },
+//         "& .MuiCardContent-root": {
+//           position: "absolute",
+//           bottom: 0,
+//           padding: muiBaseTheme.spacing.unit * 3,
+//           color: muiBaseTheme.palette.common.white,
+//           textAlign: "center",
+//           "& .MuiTypography--subheading": {
+//             lineHeight: 1.8,
+//             letterSpacing: 0.5,
+//             marginBottom: "40%"
+//           },
+//           "& .MuiTypography--explore": {
+//             marginBottom: 16,
+//             transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
+//             letterSpacing: 2
+//           }
+//         }
+//       }
+//     }
+//   }
+// });
+// NewsCard02.metadata = {
+//   name: "News Card II",
+//   description: "Best for Blog"
+// };
+
+// export default NewsCard02;
+
+//WHAT I LIKE BUT DOESNT WORK ENDS HERE
+
+
+
+
+// ADDING THE FUNCTIONALITY TO THE BUTTON
 
 // const useStyles = makeStyles({
 //   image: {
@@ -102,64 +251,7 @@ import { wrap } from 'module';
 
 // export default ParkList;
 
-
-
-
-//THIS WORKS 
-
-
-const useStyles = makeStyles({
-  image: {
-    maxHeight: 200,
-    maxWidth: 250,
-  },
-  card: {
-    maxWidth: 300,
-    height: 350,
-    margin: 10,
-    wordWrap: wrap
-  },
-  media: {
-    height: 180,
-    width: 300
-  },
-});
-
-export default function MediaCard(props) {
-  const classes = useStyles();
-  
-
-  return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image = {props.image}
-          />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {props.location}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Add to Next Adventure
-        </Button>
-        <Button size="small" color="primary">
-          Archive
-        </Button>
-      </CardActions>
-    </Card>
-  );
-  }
-
-//ENDS
-
-
+// ADDING FUNCTIONALITY ENDS HERE
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+// THIS works, need this
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import App from './App';
+// import Parklist from "./components/ParkList"
 
 // import './App.css';
 import * as serviceWorker from './serviceWorker';
@@ -14,3 +18,48 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
+
+
+//THIS DOES NOT WORK
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import JssProvider from "react-jss/lib/JssProvider";
+// import { createGenerateClassName } from "@material-ui/core/styles";
+// import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+// import "./components/styles/styles.css";
+// import ParkList from "./components/ParkList"
+// import * as serviceWorker from './serviceWorker';
+
+
+// const muiBaseTheme = createMuiTheme();
+
+// const generateClassName = createGenerateClassName({
+//     dangerouslyUseGlobalCSS: true
+//   });
+
+// function Card () {
+//   return (
+//     <JssProvider generateClassName={generateClassName}>
+//       <MuiThemeProvider
+//         theme={createMuiTheme({
+//           typography: {
+//             useNextVariants: true
+//           },
+//           overrides: ParkList.getTheme(muiBaseTheme)
+//         })}
+//       >
+//         <ParkList />
+//       </MuiThemeProvider>
+//      </JssProvider>
+//   );
+// }
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<Card />, rootElement);
+
+// serviceWorker.unregister();
